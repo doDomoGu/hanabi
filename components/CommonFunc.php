8973b5d78453d58e09b2559feacf5d9127f6248e
doDomoGu <kaka_71936410@qq.com>
2016-08-07 22:51:05 +0800

    添加常用函数文件，  增加一个补齐房间号到5位数的方法

---
+++ components/CommonFunc.php
@@ -0,0 +1,50 @@
+<?php
+namespace app\components;
+
+use yii\base\Component;
+use yii;
+
+class CommonFunc extends Component {
+    /*
+     *  根据chars 和 length 生成随机字符串
+     *  返回类型 string
+     */
+    public static function generateCode( $length = 8 ) {
+        // 字符集，可任意添加你需要的字符
+        $chars = 'klmnvXYojAB15w23LRSCD0GTUtuZMcdINOPxFHzabJpqrs4KyefghiVW67EQ89';
+        $code = '';
+        for ( $i = 0; $i < $length; $i++ )
+        {
+            // 这里提供两种字符获取方式
+            // 第一种是使用 substr 截取$chars中的任意一位字符；
+            // 第二种是取字符数组 $chars 的任意元素
+            // $code .= substr($chars, mt_rand(0, strlen($chars) – 1), 1);
+            $code .= $chars[ mt_rand(0, strlen($chars) - 1) ];
+        }
+        return $code;
+    }
+
+    public static function fixZero($num){
+        if($num<10){
+            $return = '0000'.$num;
+        }elseif($num<100){
+            $return = '000'.$num;
+        }elseif($num<1000){
+            $return = '00'.$num;
+        }elseif($num<10000){
+            $return = '0'.$num;
+        }else{
+            $return = $num;
+        }
+        return $return;
+    }
+
+    public static function mySubstr($str,$len){
+        $strlen = mb_strlen( $str, 'utf-8' );
+        if($strlen>$len-2){
+            $str = mb_substr( $str, 0, $len-1, 'utf-8' ).'...';
+        }
+
+        return $str;
+    }
+}
