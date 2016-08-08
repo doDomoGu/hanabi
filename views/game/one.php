9f55bb6c88af7b1b39816e4de83ef7085f484a4f
doDomoGu <kaka_71936410@qq.com>
2016-08-08 23:13:57 +0800

    游戏页面 样式表和js脚本 也分离

--- views/game/one.php
+++ views/game/one.php
@@ -3,7 +3,14 @@
     use app\models\Game;
 
     app\assets\AppAsset::addCssFile($this,'css/main/game/one.css');
-    app\assets\AppAsset::addJsFile($this,'js/main/game/one.js');
+
+    if($game->status==Game::STATUS_PREPARING){
+        app\assets\AppAsset::addCssFile($this,'css/main/game/_one_preparing.css');
+        app\assets\AppAsset::addJsFile($this,'js/main/game/_one_preparing.js');
+    }elseif($game->status==Game::STATUS_PLAYING){
+        app\assets\AppAsset::addCssFile($this,'css/main/game/_one_playing.css');
+        //app\assets\AppAsset::addJsFile($this,'js/main/game/_one_playing.js');
+    }
 ?>
 <div id="main">
     <div id="game_head">
