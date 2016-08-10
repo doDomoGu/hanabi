319dac687bcc2d839408b7132aedba2fe477a474
doDomoGu <kaka_71936410@qq.com>
2016-08-10 08:44:40 +0800

    增加了 按钮 调整手牌区域结构

--- views/game/_one/playing/area.php
+++ views/game/_one/playing/area.php
@@ -15,9 +15,9 @@
     $numbers = Card::$numbers;
 ?>
 <div class="top_area <?=$opposite?>_card clearfix">
-    对方 （<?=$oppositePlayer->nickname?>） 手牌
-    <div>
-        <ul class="hand_card">
+    <div class="hand_card">
+        对方 （<?=$oppositePlayer->nickname?>） 手牌
+        <ul>
             <?php foreach($cardInfo[$opposite] as $card):?>
                 <li>
                     <?=$colors[$card['color']]?> - <?=$numbers[$card['num']]?>
@@ -31,8 +31,12 @@
     <div class="middle_left">
         <div class="chess_box">
             游戏盒:
-            <div class="clue_num">线索 <span>8</span></div>
+            <div class="cue_num">线索 <span>8</span></div>
             <div class="chance_num">机会 <span>3</span></div>
+        </div>
+        <div class="chess_box_out">
+            <div class="cue_num_out">线索 <span>0</span></div>
+            <div class="chance_num_out">机会 <span>0</span></div>
         </div>
         <div class="card_library">
             牌库:
@@ -59,9 +63,9 @@
 </div>
 
 <div class="bottom_area">
-    你的手牌
-    <div>
-        <ul class="hand_card">
+    <div class="hand_card">
+        你的手牌
+        <ul>
             <?php foreach($cardInfo[$self] as $card):?>
                 <li>
                     <?/*=$colors[$card['color']]*/?><!-- - --><?/*=$numbers[$card['num']]*/?>
@@ -69,5 +73,10 @@
                 </li>
             <?php endforeach;?>
         </ul>
+    </div>
+    <div class="btns">
+        <a id="cue_btn" class="btn btn-primary">提供线索</a>
+        <a id="play_btn" class="btn btn-primary">打出一张牌</a>
+        <a id="discard_btn" class="btn btn-primary">弃掉一张牌</a>
     </div>
 </div>
