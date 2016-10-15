2803cc39b304898e19dd86e28533a91ed77cab88
doDomoGu <kaka_71936410@qq.com>
2016-10-15 21:46:31 +0800

    验证码 模型修改

--- models/VerifyCode.php
+++ models/VerifyCode.php
@@ -4,23 +4,26 @@ namespace app\models;
 
 class VerifyCode extends \yii\db\ActiveRecord
 {
-    const SCENARIO_REGISTER = 1;
-    const SCENARIO_FORGET_PASSWORD = 2;
-    const SCENARIO_CHANGE_PASSWORD = 3;
+    const SCENARIO_USER_REGISTER    = 1;  //用户注册
+    const SCENARIO_FORGET_PASSWORD  = 2;  //用户忘记密码
+    const SCENARIO_CHANGE_PASSWORD  = 3;  //用户修改密码
 
-    public static $expire_time = 5 * 60; //过期时间5分钟 即 当前时间 - 创建时间 > 5分钟 则数据过期
+    public static $expire_time; //过期时间 单位为秒; 即 当前时间 - 创建时间 > 过期时间 则验证码过期
 
+    public function __constrct(){
+        self::$expire_time = 5 * 60;
+    }
     //public static $cue_types = ['color','num'];
 
     public function attributeLabels(){
         return [
             'id' => 'ID',
-            'user_id' => '用户id',
-            'scenario' => '场景ID',
+            'user_id' => '用户id',    //对应user表的id
+            'scenario' => '场景ID',   //常量 SCENARIO_XXXX
             'code' => '验证码',
-            'msg_id' => '信息ID',
+            'msg_id' => '信息ID',     //对应msg表id
             'flag' => '是否已使用',
-            'create_time' => '创建时间',
+            'create_time' => '创建时间',//与过期时间组合得到过期时间
         ];
     }
 
