cb5db242d119748abc41fba103c73b830af7eb4f
doDomoGu <kaka_71936410@qq.com>
2016-10-18 17:47:44 +0800

    测试 command

---
+++ commands/UserController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class UserController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     */
+    public function actionIndex($message='Hello World')
+    {
+        echo $message . "\n";
+    }
+
+    public function actionSearch(array $info){
+	var_dump($info);
+echo "\n";
+	}
+}
