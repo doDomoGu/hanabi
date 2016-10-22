a9dca8af1af0fa70bc5053d6a1c354f53ef88778
doDomoGu <kaka_71936410@qq.com>
2016-10-23 03:13:26 +0800

    注册时图片验证码  不会随ajax验证而重新生成

--- components/RegCaptchaAction.php
+++ components/RegCaptchaAction.php
@@ -24,4 +24,24 @@ class RegCaptchaAction extends CaptchaAction {
 
         return $valid;
     }
+
+    public function run()
+    {
+        if (Yii::$app->request->getQueryParam(self::REFRESH_GET_VAR) !== null) {
+            // AJAX request for regenerating code
+            $code = $this->getVerifyCode(true);
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'hash1' => $this->generateValidationHash($code),
+                'hash2' => $this->generateValidationHash(strtolower($code)),
+                // we add a random 'v' parameter so that FireFox can refresh the image
+                // when src attribute of image tag is changed
+                'url' => Url::to([$this->id, 'v' => uniqid()]),
+            ];
+        } else {
+            $this->setHttpHeaders();
+            Yii::$app->response->format = Response::FORMAT_RAW;
+            return $this->renderImage($this->getVerifyCode(true));
+        }
+    }
 }
