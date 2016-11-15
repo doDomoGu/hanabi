ec03df9c1cbfc151abf3a155793003516b1080eb
doDomoGu <kaka_71936410@qq.com>
2016-11-15 15:58:55 +0800

    数据库迁移 新增  global_config 和 verify_code表

---
+++ migrations/m161115_074913_create_verify_code_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `verify_code`.
+ */
+class m161115_074913_create_verify_code_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('verify_code', [
+            'id'        =>  $this->primaryKey()->unsigned(),
+            'user_id'   =>  $this->integer(11)->unsigned()->notNull(),
+            'scenario'  =>  $this->string(40)->defaultValue(''),
+            'type'      =>  $this->smallInteger(1)->unsigned()->defaultValue(0),
+            'number'    =>  $this->string(100)->defaultValue(''),
+            'code'      =>  $this->string(10)->defaultValue(''),
+            'msg_id'    =>  $this->integer(11)->unsigned()->notNull(),
+            'flag'      =>  $this->smallInteger(1)->unsigned()->defaultValue(0)->comment('已使用 = 1'),
+            'create_time'   => $this->dateTime()
+        ],'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+    }
+
+    /*CREATE TABLE `verify_code` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) unsigned NOT NULL,
+    `scenario` varchar(40) DEFAULT '',
+    `type` tinyint(1) unsigned DEFAULT '0',
+    `number` varchar(100) DEFAULT '',
+    `code` varchar(8) DEFAULT '',
+    `msg_id` int(11) unsigned NOT NULL,
+    `flag` tinyint(1) unsigned DEFAULT '0' COMMENT '已使用 = 1',
+    `create_time` datetime,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8*/
+    public function down()
+    {
+        $this->dropTable('verify_code');
+    }
+}
---
+++ migrations/m161115_074935_create_global_config_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `global_config`.
+ */
+class m161115_074935_create_global_config_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('global_config', [
+            'id'    =>  $this->primaryKey()->unsigned(),
+            'name'  =>  $this->string(100)->notNull(),
+            'value' =>  $this->text()->notNull(),
+            'title' =>  $this->string(255)->notNull(),
+            'configable'    => $this->smallInteger(1)->notNull()->defaultValue(0)
+        ],'ENGINE=InnoDB DEFAULT CHARSET=utf8');
+    }
+
+    /*CREATE TABLE `global_config` (
+    `id` int(8) NOT NULL AUTO_INCREMENT,
+      `name` varchar(255) NOT NULL,
+      `value` text NOT NULL,
+      `title` varchar(255) NOT NULL,
+      `configable` tinyint(1) NOT NULL DEFAULT '0',
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;*/
+    public function down()
+    {
+        $this->dropTable('global_config');
+    }
+}
