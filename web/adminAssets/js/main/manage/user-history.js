1f6db1533fb7386da408ae3fd4942f28e4fce773
doDomoGu <kaka_71936410@qq.com>
2016-11-19 02:40:04 +0800

    控制台： 用户操作记录 条件检索

--- web/adminAssets/js/main/manage/user-history.js
+++ web/adminAssets/js/main/manage/user-history.js
@@ -1,7 +1,5 @@
 $(function(){
     $('#searchBtn').click(function(){
-        //$('#s_position_id').val($('#pos_id_div').html());
-        //return false;
         $('#searchForm').submit();
     })
 });
